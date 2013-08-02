31612ea7302d0388bb0016ad79f164dfca465ecf
Simon Lepkin <slepkin@gmail.com>
2013-08-02 12:19:47 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoBingo::Application.config.secret_token = 'a130d934dd5cd06ec460e59240b43dfe0a1ca9959e70f71ec9458cd11b06b612333c955d5ffb2062031ecd8427998d100bf724780a7e34c2749f3846c4ad0974'
