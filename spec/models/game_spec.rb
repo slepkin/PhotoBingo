892ca87db9bbe3309ad6a2c6b158592d06aee300
Simon Lepkin <Simon@appAcademy.io>
2013-08-15 15:25:16 -0400

    Tests reset database before each test! Hooray

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Game do
-  before(:all) do
+  before(:each) do
     user = User.create!(:name => "tester", :email => "tester2@blah.com",
   :password => "password", :password_confirmation => "password")
   #Use Factory Girl for this
@@ -13,42 +13,43 @@ describe Game do
     theme.save
 
     game = Game.new(theme_id: theme.id)
-  end
 
-  describe ".new" do
-    it "cannot be saved without multiple players" do
-      game.save
-      game.errors.full_messages.should_not eq([])
+
+    describe ".new" do
+      it "cannot be saved without multiple players" do
+        game.save
+        game.errors.full_messages.should_not eq([])
+      end
+
+      it "can be saved with multiple players" do
+        game.users = User.all
+        game.save
+        game.errors.full_messages.should eq([])
+      end
     end
 
-    it "can be saved with multiple players" do
-      game.users = User.all
-      game.save
-      game.errors.full_messages.should eq([])
-    end
-  end
+    describe "#new_notifications?" do
+      before(:each) do
+        game = Game.new(theme_id: theme.id)
+        game.users = User.all
+        game.save
+      end
 
-  describe "#new_notifications?" do
-    before(:all) do
-      game = Game.new(theme_id: theme.id)
-      game.users = User.all
-      game.save
-    end
+      it "is false when no notifications" do
+        expect(game.new_notifications?(user)).to be_false
+      end
 
-    it "is false when no notifications" do
-      expect(game.new_notifications?(user)).to be_false
-    end
+      it "is true if any notifications are new" do
+        Notification.create!(game_id: game.id, subject_id: user.id, quality: "win")
+        expect(game.new_notifications?(user)).to be_true
+      end
 
-    it "is true if any notifications are new" do
-      Notification.create!(game_id: game.id, subject_id: user.id, quality: "win")
-      expect(game.new_notifications?(user)).to be_true
-    end
+      it "is false when notifications exist but aren't new" do
+        Visit.create!(user_id: user.id, game_id: game.id)
+        expect(game.new_notifications?(user)).to be_false
+      end
 
-    it "is false when notifications exist but aren't new" do
-      Visit.create!(user_id: user.id, game_id: game.id)
-      expect(game.new_notifications?(user)).to be_false
     end
-
   end
 
 end
--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -7,61 +7,61 @@ describe Theme do
   #Use Factory Girl for this
 
     theme = Theme.new(name: "test theme", user_id: user.id)
-  end
 
-  describe ".new" do
 
-    it "makes a Theme" do
-      theme.should be_a(Theme)
+    describe ".new" do
+
+      it "makes a Theme" do
+        theme.should be_a(Theme)
+      end
+
+      it "has given name" do
+        theme.name.should eq("test theme")
+      end
+      it "belongs to author" do
+        theme.user.should eq(user)
+      end
+
     end
 
-    it "has given name" do
-      theme.name.should eq("test theme")
-    end
-    it "belongs to author" do
-      theme.user.should eq(user)
-    end
+    describe "#save" do
+      context "without name" do
+        it "impossible" do
+          theme.name = nil
+          theme.save
+          theme.errors.full_messages.should_not eq([])
+        end
+      end
 
-  end
+      context "without user" do
+        it "impossible without user" do
+          theme.user = nil
+          theme.save
+          theme.errors.full_messages.should_not eq([])
+        end
+      end
 
-  describe "#save" do
-    context "without name" do
-      it "impossible" do
-        theme.name = nil
+      it "impossible without phrases" do
         theme.save
         theme.errors.full_messages.should_not eq([])
       end
-    end
 
-    context "without user" do
-      it "impossible without user" do
-        theme.user = nil
+      it "impossible with <16 phrases" do
+        rand(15).times do
+          theme.phrases.new(body: "I'm a body")
+        end
         theme.save
         theme.errors.full_messages.should_not eq([])
       end
-    end
 
-    it "impossible without phrases" do
-      theme.save
-      theme.errors.full_messages.should_not eq([])
-    end
-
-    it "impossible with <16 phrases" do
-      rand(15).times do
-        theme.phrases.new(body: "I'm a body")
+      it "works with 16 phrases" do
+        16.times do
+          theme.phrases.new(body: "I'm a body")
+        end
+        theme.save
+        theme.errors.full_messages.should eq([])
       end
-      theme.save
-      theme.errors.full_messages.should_not eq([])
-    end
 
-    it "works with 16 phrases" do
-      16.times do
-        theme.phrases.new(body: "I'm a body")
-      end
-      theme.save
-      theme.errors.full_messages.should eq([])
     end
-
   end
-
 end
