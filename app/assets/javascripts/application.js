2907245f0fa3bd5e9dfba417746ae2f1a51304cb
Simon Lepkin <Simon@appAcademy.io>
2013-08-16 10:15:58 -0400

    Added button for more phrase fields

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,14 +15,19 @@
 //= require_tree
 
 $(document).ready(function() {
-  $('#more-phrases').click(function(){
+  $('#more-phrases').click(function(event){
+    event.preventDefault();
     var initial = $('#phrase-list li').length;
-    $.get({
+    $.ajax({
       url: "/phrases/new",
       success: function(listItem) {
-        var parsedListItem = $(listItem)
-        $('#phrase-list').append()
+        for(var i=initial;i<initial+3;i++){
+          var parsed = $(listItem);
+          parsed.children('.phrase-field').attr('name', 'theme[phrases_attributes]['+i+'][body]');
+          parsed.children('.primary-key').attr('name', 'theme[phrases_attributes]['+i+'][id]');
+          $('#phrase-list').append(parsed);
+        }
       }
-    })
-  })
-}
+    });
+  });
+})
