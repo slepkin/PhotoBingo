c2ae8f24bce7ede7a30edbdac6dd407f05e11db3
Simon Lepkin <slepkin@gmail.com>
2013-08-19 14:56:10 -0400

    Moves JS around

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,20 +14,3 @@
 //= require jquery_ujs
 //= require_tree
 
-$(document).ready(function() {
-  $('#more-phrases').click(function(event){
-    event.preventDefault();
-    var initial = $('#phrase-list li').length;
-    $.ajax({
-      url: "/phrases/new",
-      success: function(listItem) {
-        for(var i=initial;i<initial+3;i++){
-          var parsed = $(listItem);
-          parsed.children('.phrase-field').attr('name', 'theme[phrases_attributes]['+i+'][body]');
-          parsed.children('.primary-key').attr('name', 'theme[phrases_attributes]['+i+'][id]');
-          $('#phrase-list').append(parsed);
-        }
-      }
-    });
-  });
-})
